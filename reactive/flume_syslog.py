adc5785a4f37955b15778b1fef2471740db8e6bb
Cory Johns <johnsca@gmail.com>
2016-02-29 19:35:33 -0500

    Improved status reporting

--- reactive/flume_syslog.py
+++ reactive/flume_syslog.py
@@ -1,5 +1,5 @@
 from charms.reactive import when, when_not
-from charms.reactive import set_state, remove_state
+from charms.reactive import set_state, remove_state, is_state
 from charms.reactive.helpers import any_file_changed
 
 from charmhelpers.core import hookenv
@@ -8,19 +8,23 @@ from charms.layer.apache_flume_base import Flume
 
 
 @when('flume-base.installed')
-@when_not('flume-sink.joined')
-def waiting_for_flume_connection():
-    hookenv.status_set('blocked', 'Waiting for connection to Flume Sink')
+@when_not('flume-syslog.started')
+def report_status():
+    syslog_joined = is_state('syslog.joined')
+    sink_joined = is_state('flume-sink.joined')
+    sink_ready = is_state('flume-sink.ready')
+    if not syslog_joined and not sink_joined:
+        hookenv.status_set('blocked', 'Waiting for connection to Flume Sink and Syslog Forwarder')
+    elif not syslog_joined:
+        hookenv.status_set('blocked', 'Waiting for connection to Syslog Forwarder')
+    elif not sink_joined:
+        hookenv.status_set('blocked', 'Waiting for connection to Flume Sink')
+    elif sink_joined and not sink_ready:
+        hookenv.status_set('blocked', 'Waiting for Flume Sink')
 
 
-@when('flume-base.installed', 'flume-sink.joined')
-@when_not('flume-sink.ready')
-def waiting_for_flume_available(sink):  # pylint: disable=unused-argument
-    hookenv.status_set('blocked', 'Waiting for Flume Sink')
-
-
-@when('flume-base.installed', 'flume-sink.ready')
-def configure_flume(sink):
+@when('flume-base.installed', 'flume-sink.ready', 'syslog.joined')
+def configure_flume(sink, syslog):  # pylint: disable=unused-argument
     hookenv.status_set('maintenance', 'Configuring Flume')
     flume = Flume()
     flume.configure_flume({'agents': sink.agents()})
@@ -36,3 +40,9 @@ def stop_flume():
     flume = Flume()
     flume.stop()
     remove_state('flume-syslog.started')
+
+
+@when('flume-syslog.started')
+@when_not('syslog.joined')
+def lost_syslog():
+    stop_flume()
